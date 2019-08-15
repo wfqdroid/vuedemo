8b234c8a677211235dfba85bf37df7b21084611a
wfq <946586142@qq.com>
2019-08-15 09:41:54 +0800

    第一次修改

---
+++ src/views/Demo1.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>Demo1</div>
+</template>
+
+<script>
+    export default {
+        name: "demo1",
+        data() {
+            return {}
+        },
+        methods: {},
+        mounted() {
+            let str = '第一次修改'
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
